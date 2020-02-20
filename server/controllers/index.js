889f1303bf5c1471a2d4b4c0ad813a9806c554ee
DanielAdek <maildaniel.me1@gmail.com>
2020-02-20 23:59:19 +0100

    feat: implement CRUD operation on product

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,6 @@
 import User from './user';
+import Product from './product';
 
 export {
-  User
+  User, Product
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import UserOperations from './user';
+import ProductOperations from './product';
 
 
 const router = Router();
 
 router.use('/users', UserOperations);
+router.use('/product', ProductOperations);
 
 export default router;
